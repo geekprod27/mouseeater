945cf2f6281573f75eabef548b7c0e1040b21009
Nathan FELSEMBERG <natfeljaf@gmail.com>
2024-05-22 18:45:14 +0200

    fix seg fault

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use mouce::Mouse;
-use std::{thread, time::{self, Duration}};
+use std::{thread, time::{self}};
 use rand::Rng;
 
 fn calculer_angle(x1: f64,y1: f64,x2: f64,y2: f64) -> f64{
@@ -10,36 +10,17 @@ fn calculer_angle(x1: f64,y1: f64,x2: f64,y2: f64) -> f64{
     return angle;
 }
 
-fn move_to_angle(angle: f64, dist: f64, initial_x: f64, initial_y: f64) {
-    let new_x = initial_x + dist * angle.cos();
-    let new_y = initial_y + dist * angle.sin();
-    let mouse_manager = Mouse::new();
-    mouse_manager.move_to(new_x as usize, new_y as usize).expect("Never gonna");
-}
 
-fn crasheur(xcrash: f64, ycrash: f64) {
-    let mouse_manager = Mouse::new();
-    match mouse_manager.get_position() {
-        Ok((x, y)) => {
-            let angle = calculer_angle(x.into(), y.into(), xcrash, ycrash);
-            for i in 0..200 {
-                move_to_angle(angle, (i*2) as f64, x.into(), y.into());
-                thread::sleep(time::Duration::from_millis(1));
-            }
-        }
-        Err(error) => println!("{error}")
-    }
-}
 
 fn init_eater(xcrash: f64, ycrash: f64)
 {
     println!("Init eater at x {} y {}", xcrash, ycrash)
 }
 
-fn move_eater(xcrash: f64, ycrash: f64)
-{
-    println!("eater at x {} y {}", xcrash, ycrash)
-}
+// fn move_eater(xcrash: f64, ycrash: f64)
+// {
+//     println!("eater at x {} y {}", xcrash, ycrash)
+// }
 
 fn main() {
     let mut eater_x:f64 = 1000.0;
@@ -47,6 +28,7 @@ fn main() {
     let mut status = 1; // 1 = charger la sourie 2 = balade
     init_eater(eater_x, eater_y);
     let mouse_manager = Mouse::new();
+    let mut rng = rand::thread_rng();
     loop {
         match status {
             1=> { // charger la sourie
@@ -57,7 +39,7 @@ fn main() {
                             let angle = calculer_angle(eater_x, eater_y, x.into(), y.into());
                             eater_x = eater_x + (-10.0) * angle.cos();
                             eater_y = eater_y + (-10.0) * angle.sin();
-                            move_eater(eater_x, eater_y);
+                            // move_eater(eater_x, eater_y);
                             
                             dist = ((eater_x - x as f64).powi(2) + (eater_y - y as f64).powi(2)).sqrt();
                             thread::sleep(time::Duration::from_millis(10));
@@ -65,11 +47,21 @@ fn main() {
                         Err(error) => println!("{error}")
                     }
                 }
-                crasheur(eater_x, eater_y);
+                match mouse_manager.get_position() {
+                    Ok((x, y)) => {
+                        let angle = calculer_angle(x.into(), y.into(), eater_x, eater_y);
+                        for i in 0..200 {
+                            let new_x = x as f64+ (i*2) as f64 * angle.cos();
+                            let new_y = y as f64+ (i*2) as f64 * angle.sin();
+                            mouse_manager.move_to(new_x as usize, new_y as usize).expect("Never gonna");
+                            thread::sleep(time::Duration::from_millis(1));
+                        }
+                    }
+                    Err(error) => println!("{error}")
+                }
                 status = 2;
             }
             2=> {
-                let mut rng = rand::thread_rng();
                 let target_x:f64 = rng.gen_range(10.0, 2000.0);
                 let target_y:f64 = rng.gen_range(10.0, 2000.0);
 
